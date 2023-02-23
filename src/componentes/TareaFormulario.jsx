55f629ff8a2a2442dabddb4deaae0d5c5d192723
marcosc1995 <marcos_c95@outlook.com>
2023-02-23 15:54:24 -0300

    app terminada

---
+++ src/componentes/TareaFormulario.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import "../hojas-de-estilo/TareaFormulario.css";
+import { v4 as uuidv4 } from "uuid";
+
+function TareaFormulario(props) {
+  const [input, setInput] = useState("");
+
+  const manejarCambio = (e) => {
+    setInput(e.target.value);
+  };
+  const manejarEnvio = (e) => {
+    e.preventDefault();
+    console.log("enviando formulario");
+    const tareaNueva = {
+      id: uuidv4(),
+      texto: input,
+      completada: false,
+    };
+    props.onSubmit(tareaNueva);
+  };
+
+  return (
+    <form className="tarea-formulario" onSubmit={manejarEnvio}>
+      <input
+        className="tarea-input"
+        placeholder="Escribe una Tarea"
+        name="texto"
+        type="text"
+        onChange={manejarCambio}
+      />
+      <button className="tarea-boton">Agregar Tarea</button>
+    </form>
+  );
+}
+export default TareaFormulario;
